0af58df07827e85f050a61f6054a9f2504334a3b
cs <bartus.csongor@gmail.com>
2014-07-14 18:41:05 +0300

    The jQuery script refactored and documented

--- assets/jquery.gravity-point.js
+++ assets/jquery.gravity-point.js
@@ -5,27 +5,43 @@ $(document).ready(function() {
   
   
   // How it works:
-  // 1. Draws a vertical, a horizontal axis around the page centre together with a centered golden ration rectangle
-  // 2. Calculates and draws the gravity point based on those elements which are currently visible on the page, and defined in the .html
+  // 1. Calculates and draws the gravity point based on those elements which are currently visible on the page, and defined in the .html
+  // 2. Draws a vertical, a horizontal axis around the page centre together with a centered golden ration rectangle and the gravity point
   // 3. Repeats 1. and 2. on scroll and browser resize
   
   
   
-  // This is the main function
+  // 1. Calculate the gravity point
+  // ----
   function drawGravityPoint() {
     
-    // Check if there are elements defined in the .html file 
+    // Check if the elements are defined in the .html file
+    // - these elements will be taken into consideration when calculating the gravity point
     if (typeof gravityPointElements == "undefined") {
       console.log("gravityPointElements is undefined in the HTML");
       return;
     }
+    
+    // The algorithm
+    // - calculate the weight of each element, wich is the area of the element * the tone of the element (40% for text, 70% for image). Add these weights into a variable (sum) 
+    // - calculate the distance of the centre of the element from the centre of the page
+    // - add these distances (sumX, sumY)
+    // - the position of the gravity point is: half of the screen + sumX / sum on horizontal, half of the screen - sumY / sum on vertical
+    
+    // The elements of the gravity point
+    // - they are defined in the .html as JSON
+    // - there can be singular elements (ex. #header) or a collection of elements (ex. .articles)
+    // - example:
+    //  - a H1 element and all the images
+    // <script>
+    //  var gravityPointElements = '[{"id" : "h1", "type" : "text"},{"id" : "img", "type" : "image", "collection" : "true"}]';
+    // </script>
     
     
     // Set up variables for the arithmetic mean calculations
     var sum = 0;
     var sumX = 0;
     var sumY = 0;
-    
     
     
     // Loop through elements
@@ -67,7 +83,8 @@ $(document).ready(function() {
       }
     });
     
-    // append the results to the end of the .html
+    // 2. append the results to the end of the .html
+    // ---
     appendToPage(sumX, sumY, sum);
   }
   
@@ -79,28 +96,15 @@ $(document).ready(function() {
   
   
   
+  
+  
   // Helper functions
   // ----
   
   
   
-  
-  // Calculate
-  // - only elements in the viewport are taken into consioderation
-  function calculate(item, type, sum, sumX, sumY) {
-    if (isElementInViewport(item)) {
-      var weight = getElementWeight(item, type);
-      var x = distanceX(item);
-      var y = distanceY(item);
-      
-      sum += weight;
-      sumX += weight * x;
-      sumY += weight * y;
-    }
-  }
-  
-  
-  // Get the distance of the center of an element from origo
+ 
+  // Get the distance of the center of an element from the centre of the page
   // - for X
   function distanceX(item) {
     var w = item.css('width');
@@ -124,13 +128,15 @@ $(document).ready(function() {
   }
   
   
-  // Get a single elements weight
+  // Get a single element's weight
   function getElementWeight(item, type) {
     var w = item.css('width');
     var h = item.css('height');
     return parseInt(w) * parseInt(h) * getGravityByType(type);
   }
   
+  // Get a single element's tone
+  // text: 40%, image: 70%
   function getGravityByType(type) {
     return (type == 'text') ? 0.4 : 0.7;
   }
@@ -156,14 +162,37 @@ $(document).ready(function() {
   }
   
   
-  // The axis and the rectangle
+  
+  // Add all the results to the HTML
+  function appendToPage(sumX, sumY, sum) {   
+    // A special section will hold all the gravity markup together
+    $('body').append('<section id="gravity">');
+    
+    // Draw the guides and the rectangle
+    drawGuides();
+    drawGoldenRectangle();
+    
+    // Draw the gravity point
+    var x = $(window).width() / 2 + sumX / sum;
+    var y = $(window).height() / 2 - sumY / sum;
+    var style = 'left: ' + x + 'px; top: ' + y + 'px';
+    $('#gravity').append('<div class="h-gravity-point" style="' + style + '"></div>');
+    
+    $('body').append('</section>');
+  }
+  
+  
+  
+  // Draw the axis and the rectangle holders
   function drawGuides() {
     $('#gravity').append('<div class="h-horizontal"></div>');
     $('#gravity').append('<div class="h-vertical"></div>');
     $('#gravity').append('<div class="h-golden-rectangle"></div>');
   }
   
-  // The golden ratio sized rectangle around the page centre
+  
+  
+  // Draw the golden ratio sized rectangle around the page centre
   function drawGoldenRectangle() {
     var minorX = $(window).width() / 8 * 3;
     var majorX = $(window).width() / 8 * 5;
